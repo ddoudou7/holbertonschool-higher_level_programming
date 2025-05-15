2cb212d6b7eb285e1d118ebd0013f1772d5835b8
Mr Philips <ddoudou7@yahoo.fr>
2025-05-15 14:15:45 +0000

    Update docstring of add_integer to meet checker requirement

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,11 @@ This module provides a function to add two integers.
 """
 
 def add_integer(a, b=98):
-    """Return the sum of a and b, after validating and casting to integers."""
+    """
+    Return the sum of a and b.
+
+    Both values must be integers or floats. Floats are cast to ints.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
