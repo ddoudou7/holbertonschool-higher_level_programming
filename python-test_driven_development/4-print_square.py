72c49e0a52a70f64dbae8c988e9b8d2bc7346fe9
Mr Philips <ddoudou7@yahoo.fr>
2025-05-16 15:24:15 +0000

    Fix: added missing blank line to comply with pycodestyle E302

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,6 +2,8 @@
 """
 This module defines the function print_square which prints a square with "#"
 """
+
+
 def print_square(size):
     """Prints a square with "#" of size `size`."""
     if type(size) is not int:
