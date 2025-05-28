fa42f0a6abef840d119abbebe7afe0717d234ef6
Mr Philips <ddoudou7@yahoo.fr>
2025-05-28 10:40:45 +0000

    Fix: Task 1 - implement MyList with print_sorted()

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This module defines a class MyList that inherits from list.
+This module defines a class MyList that inherits from list and adds a method to print the list sorted.
 """
 
 class MyList(list):
-    """A subclass of list with a method to print the list sorted."""
+    """Subclass of list with a sorted print method."""
 
     def print_sorted(self):
-        """Print the list in ascending sorted order without modifying the original list."""
+        """Print the list sorted in ascending order (without modifying it)."""
         print(sorted(self))
 
