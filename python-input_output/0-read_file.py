7779af2c9c09c9416f4f8a45a018561b831cc73f
Mr Philips <ddoudou7@yahoo.fr>
2025-06-04 16:31:15 +0000

    Task 0: Read file using with statement

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 0-read_file
+Reads and prints the contents of a text file (UTF8).
+"""
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): Name of the file to read.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
