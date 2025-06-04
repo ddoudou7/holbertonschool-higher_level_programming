7f18c7775bba1444d1c3881eb1d50a8318716c54
Mr Philips <ddoudou7@yahoo.fr>
2025-06-04 16:57:38 +0000

    Task 4: From JSON string to Object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 4-from_json_string
+Returns the Python object from a JSON string.
+"""
+
+import json
+
+def from_json_string(my_str):
+    return json.loads(my_str)
