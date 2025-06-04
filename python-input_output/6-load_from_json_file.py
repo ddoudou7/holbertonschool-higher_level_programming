364b16002acad58ef404284a80c8b495de10162a
Mr Philips <ddoudou7@yahoo.fr>
2025-06-04 18:11:02 +0000

    Task 6: Create object from a JSON file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+"""
+6-load_from_json_file.py
+"""
+
 import json
 
 def load_from_json_file(filename):
-    """Create an object from a JSON file."""
-    with open(filename, "r") as f:
-        return json.load(f)
+    """
+    Crée un objet Python à partir du contenu JSON d'un fichier.
 
+    Args:
+        filename (str): Nom du fichier JSON à ouvrir.
+
+    Returns:
+        L'objet Python obtenu après désérialisation du JSON.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
