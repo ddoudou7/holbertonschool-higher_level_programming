4311c9c73434addb8367f0bcffc29f198e769409
Mr Philips <ddoudou7@yahoo.fr>
2025-06-04 18:19:11 +0000

    Task 4: From JSON string to Object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """
-Module 4-from_json_string
-Returns the Python object from a JSON string.
+4-from_json_string.py
 """
 
 import json
 
 def from_json_string(my_str):
+    """
+    Retourne l’objet Python représenté par la chaîne JSON my_str.
+
+    Args:
+        my_str (str): Chaîne JSON à désérialiser.
+
+    Returns:
+        L’objet Python (list, dict, etc.) correspondant au JSON.
+    """
     return json.loads(my_str)
