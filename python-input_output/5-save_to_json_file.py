8c0e04fb65a0a9a446ce641864fcaab5df3b53ba
Mr Philips <ddoudou7@yahoo.fr>
2025-06-04 18:16:50 +0000

    Task 5: Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""
+5-save_to_json_file.py
+"""
+
 import json
 
 def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
+    """
+    Écrit l’objet my_obj dans le fichier filename
+    en utilisant sa représentation JSON.
+
+    Args:
+        my_obj: Objet Python à sérialiser (liste, dict, etc.).
+        filename: Nom du fichier où écrire le JSON.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
