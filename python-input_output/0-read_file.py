aa9a8a1d4e06cdb241775175078b2bde9fe9fe7f
Mr Philips <ddoudou7@yahoo.fr>
2025-06-05 18:41:52 +0000

    Task 0: Read file content and print it to stdout with doctest

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Module for reading and printing the contents of a file."""
+
 
 def read_file(filename=""):
+    """Reads a text file and prints its contents to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
