ca4ef28e917cdf4c7d12f3d7f876eee40611e7a6
Mr Philips <ddoudou7@yahoo.fr>
2025-06-05 17:50:55 +0000

    Task 1: Pickling Custom Classes with serialize and deserialize

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module for pickling custom classes"""
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Is Student:", self.is_student)
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except Exception:
+            pass
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except Exception:
+            return None
