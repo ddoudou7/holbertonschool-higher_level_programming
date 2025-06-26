ab0953fca1a893b893302b8348adacc75f1b692e
Mr Philips <ddoudou7@example.com>
2025-06-26 16:18:19 +0200

    Task 0 fix: comment before every query + IF NOT EXISTS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,13 @@
+-- 0-privileges.sql
+
+-- create user_0d_1 if missing
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '';
+
+-- show grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- create user_0d_2 if missing
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY '';
+
+-- show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
