f9e7bd0e9a5512bda3b9f46a35fba74dfeb85fd3
Mr Philips <ddoudou7@example.com>
2025-06-26 15:00:03 +0200

    Task 6: create DB hbtn_0d_usa and table states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- 6-states.sql
+-- Creates DB hbtn_0d_usa and table states(id PK AI, name NOT NULL)
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+  id   INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
