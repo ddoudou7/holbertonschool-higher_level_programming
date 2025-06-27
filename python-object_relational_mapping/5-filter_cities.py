a8085b93594021ce5d16c7c87e05424d9dbfa891
Mr Philips <ddoudou7@yahoo.fr>
2025-06-27 11:40:48 +0000

    Task 5: list all cities of a given state (MySQLdb, injection-safe)

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Takes a state name as argument and lists all its cities in hbtn_0e_4_usa.
+Takes a state name as argument and lists all cities of that state
+from the database hbtn_0e_4_usa.
 
 Usage:
     ./5-filter_cities.py <mysql_user> <mysql_pwd> <db_name> <state_name>
@@ -12,29 +13,30 @@ def main():
     if len(sys.argv) != 5:
         sys.exit(1)
 
-    user, pwd, db_name, state = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    user, pwd, db, state = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=user,
         passwd=pwd,
-        db=db_name,
-        charset="utf8",
-        use_unicode=True
+        db=db,
+        charset="utf8"
     )
     cur = conn.cursor()
-    cur.execute(
+    query = (
         "SELECT cities.name "
-        "FROM cities JOIN states ON cities.state_id = states.id "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
         "WHERE states.name = %s "
-        "ORDER BY cities.id ASC",
-        (state,)
+        "ORDER BY cities.id ASC"
     )
+    cur.execute(query, (state,))
     rows = cur.fetchall()
-    conn.close()
-
     if rows:
-        print(", ".join(r[0] for r in rows))
+        print(", ".join([r[0] for r in rows]))
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
     main()
