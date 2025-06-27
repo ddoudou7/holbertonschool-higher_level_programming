6eaf54522ee2402eda6d4699f141ee3fc83358ae
Mr Philips <ddoudou7@yahoo.fr>
2025-06-27 11:07:05 +0000

    Task 10: get State by name via SQLAlchemy

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 """
-Prints the id of the State matching argv[4] (safe query).
-Usage: ./10-model_state_my_get.py <user> <pwd> <db> <state_name>
+Prints the State.id of the State object with the name passed as argument
+from the database hbtn_0e_6_usa. Safe from SQL injection.
+
+Usage:
+    ./10-model_state_my_get.py <mysql_user> <mysql_pwd> <db_name> <state_name>
+
+If the state is not found, prints "Not found".
 """
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+def main():
+    if len(sys.argv) != 5:
+        sys.exit(1)
 
-    with Session(engine) as session:
-        state = session.query(State).filter(State.name == sys.argv[4]).first()
-        print(state.id if state else "Not found")
+    user, pwd, db, target = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}',
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.name == target).first()
+    session.close()
+
+    if result:
+        print(result.id)
+    else:
+        print("Not found")
+
+if __name__ == "__main__":
+    main()
