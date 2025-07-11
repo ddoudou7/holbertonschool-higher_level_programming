b2082e33bc64e00e3f193bb3f7c2cc6ff8b4e0b0
Mr Philips <ddoudou7@yahoo.fr>
2025-07-11 18:01:45 +0000

    11: second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,18 @@
 #!/usr/bin/node
 
-const len = process.argv.length;
-const nums = process.argv.slice(2).map(function (n) {
-  return parseInt(n);
-});
-const max = Math.max.apply(Math, nums);
-const min = Math.min.apply(Math, nums);
-
-if (len > 3) {
-  let i = 0;
-  let n = 0;
-  let secBig = min;
-
-  for (; i < len; ++i) {
-    n = nums[i];
-
-    if (n > secBig && n < max) {
-      secBig = n;
+const nums = process.argv.slice(2).map(n => parseInt(n));
+if (nums.length < 2) {
+  console.log(0);
+} else {
+  let max1 = -Infinity;
+  let max2 = -Infinity;
+  for (const n of nums) {
+    if (n > max1) {
+      max2 = max1;
+      max1 = n;
+    } else if (n > max2 && n < max1) {
+      max2 = n;
     }
   }
-
-  console.log(secBig);
-} else {
-  console.log(0);
+  console.log(max2);
 }
