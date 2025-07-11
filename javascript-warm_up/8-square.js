4a1167b6b7929691f9341cac7e82f6890cd4e8fc
Mr Philips <ddoudou7@yahoo.fr>
2025-07-11 17:56:14 +0000

    8: square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
 
 const size = parseInt(process.argv[2]);
-
-if (size) {
-  for (let i = 0; i < size; ++i) {
-    let j = 0;
-
-    for (; j < size; ++j) {
-      process.stdout.write('X');
-    }
-
-    if (j === size) {
-      console.log('');
-    }
-  }
-} else {
+if (isNaN(size)) {
   console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
