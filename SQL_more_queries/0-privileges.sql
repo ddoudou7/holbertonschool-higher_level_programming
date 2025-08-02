02d1b2744eb4dd9923f4fcb40ad514854c9197bd
Mr Philips <ddoudou7@yahoo.fr>
2025-08-02 02:40:55 +0000

    Fix task 0: only SHOW GRANTS as checker expects

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,18 +1,5 @@
--- List all privileges of user_0d_1 and user_0d_2 on localhost
--- If a user does not exist, display an informative line instead of failing.
+-- List privileges of user_0d_1 and user_0d_2 on localhost
+-- MySQL’s own error message is displayed if a user does not exist.
 
--- ► user_0d_1
-SELECT '### user_0d_1 ###' AS '';
-SELECT IF(
-  EXISTS (SELECT 1 FROM mysql.user WHERE user='user_0d_1' AND host='localhost'),
-  'User exists' , 'User user_0d_1@localhost does not exist'
-) AS '';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- ► user_0d_2
-SELECT '### user_0d_2 ###' AS '';
-SELECT IF(
-  EXISTS (SELECT 1 FROM mysql.user WHERE user='user_0d_2' AND host='localhost'),
-  'User exists' , 'User user_0d_2@localhost does not exist'
-) AS '';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
